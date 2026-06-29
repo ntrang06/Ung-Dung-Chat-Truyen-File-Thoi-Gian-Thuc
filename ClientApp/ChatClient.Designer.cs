330995ba2ac9e8394e9abc231ad7609f075b6136
Nguyen Thi Thao Nhu <nhuntt0635@ut.edu.vn>
2026-06-29 07:20:48 +0700

    commit lên thì m ghi là:  fix back button event handling and remove invalid object.Show() call

--- ClientApp/ChatClient.Designer.cs
+++ ClientApp/ChatClient.Designer.cs
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -32,12 +33,10 @@
             this.btnSendFile = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.txtMessage = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.btnConnect = new System.Windows.Forms.Button();
-            this.txtPort = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtIP = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.btnBackToMenu = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSend
@@ -45,10 +44,10 @@
             this.btnSend.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.btnSend.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.btnSend.Location = new System.Drawing.Point(690, 384);
+            this.btnSend.Location = new System.Drawing.Point(448, 365);
             this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(92, 29);
+            this.btnSend.Size = new System.Drawing.Size(79, 36);
             this.btnSend.TabIndex = 9;
             this.btnSend.Text = "Gửi";
             this.btnSend.UseVisualStyleBackColor = false;
@@ -56,11 +55,11 @@
             // btnSendFile
             // 
             this.btnSendFile.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.btnSendFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.btnSendFile.Location = new System.Drawing.Point(596, 384);
+            this.btnSendFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendFile.Location = new System.Drawing.Point(547, 365);
             this.btnSendFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSendFile.Name = "btnSendFile";
-            this.btnSendFile.Size = new System.Drawing.Size(88, 29);
+            this.btnSendFile.Size = new System.Drawing.Size(98, 36);
             this.btnSendFile.TabIndex = 9;
             this.btnSendFile.Text = "Gửi File";
             this.btnSendFile.UseVisualStyleBackColor = false;
@@ -69,11 +68,11 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.SystemColors.Info;
-            this.label4.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 11F);
+            this.label4.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label4.Location = new System.Drawing.Point(245, 21);
+            this.label4.Location = new System.Drawing.Point(182, 20);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(255, 26);
+            this.label4.Size = new System.Drawing.Size(294, 31);
             this.label4.TabIndex = 10;
             this.label4.Text = "KHUNG CHAT CLIENT";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -81,95 +80,68 @@
             // txtMessage
             // 
             this.txtMessage.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.txtMessage.Location = new System.Drawing.Point(15, 385);
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(23, 369);
             this.txtMessage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(565, 22);
+            this.txtMessage.Size = new System.Drawing.Size(403, 32);
             this.txtMessage.TabIndex = 6;
-            this.txtMessage.Text = "Nhập tin nhắn";
             this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
             // 
-            // listBox1
+            // rtbMessages
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(15, 98);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(767, 260);
-            this.listBox1.TabIndex = 8;
+            this.rtbMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbMessages.Location = new System.Drawing.Point(23, 101);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.ReadOnly = true;
+            this.rtbMessages.Size = new System.Drawing.Size(622, 229);
+            this.rtbMessages.TabIndex = 11;
+            this.rtbMessages.Text = "";
             // 
-            // btnConnect
+            // btnBackToMenu
             // 
-            this.btnConnect.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.btnConnect.Location = new System.Drawing.Point(711, 52);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(71, 23);
-            this.btnConnect.TabIndex = 4;
-            this.btnConnect.Text = "Kết nối";
-            this.btnConnect.UseVisualStyleBackColor = true;
-            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            this.btnBackToMenu.BackColor = System.Drawing.SystemColors.Info;
+            this.btnBackToMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackToMenu.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBackToMenu.Location = new System.Drawing.Point(11, 11);
+            this.btnBackToMenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBackToMenu.Name = "btnBackToMenu";
+            this.btnBackToMenu.Size = new System.Drawing.Size(118, 40);
+            this.btnBackToMenu.TabIndex = 13;
+            this.btnBackToMenu.Text = "Quay lại";
+            this.btnBackToMenu.UseVisualStyleBackColor = false;
+            this.btnBackToMenu.Click += new System.EventHandler(this.btnBackToMenu_Click);
             // 
-            // txtPort
+            // panel1
             // 
-            this.txtPort.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.txtPort.Location = new System.Drawing.Point(573, 53);
-            this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(96, 22);
-            this.txtPort.TabIndex = 3;
-            this.txtPort.Text = "5000";
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(480, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 16);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Cổng (Port):";
-            // 
-            // txtIP
-            // 
-            this.txtIP.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.txtIP.Location = new System.Drawing.Point(91, 59);
-            this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(96, 22);
-            this.txtIP.TabIndex = 1;
-            this.txtIP.Text = "127.0.0.1";
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Địa chỉ IP: ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.rtbMessages);
+            this.panel1.Controls.Add(this.btnBackToMenu);
+            this.panel1.Controls.Add(this.txtMessage);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Controls.Add(this.btnSendFile);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 453);
+            this.panel1.TabIndex = 14;
             // 
             // lstChat
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(799, 450);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.btnSendFile);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.txtMessage);
-            this.Controls.Add(this.btnConnect);
-            this.Controls.Add(this.txtPort);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtIP);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(673, 453);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "lstChat";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.lstChat_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -178,12 +150,8 @@
         private System.Windows.Forms.Button btnSendFile;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtMessage;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button btnConnect;
-        private System.Windows.Forms.TextBox txtPort;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtIP;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbMessages;
+        private System.Windows.Forms.Button btnBackToMenu;
+        private System.Windows.Forms.Panel panel1;
     }
 }
-
--- ClientApp/ChatClient.cs
+++ ClientApp/ChatClient.cs
@@ -8,60 +8,82 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.Sockets;
-using System.IO; // <-- Thêm thư viện này để đọc/ghi File
+using System.IO;
+using System.Threading; // <-- BẮT BUỘC thêm thư viện này để quản lý luồng Thread
 
 namespace ClientApp
 {
-    // Giữ nguyên tên lớp là lstChat theo đúng hình chụp hiện tại của bạn
     public partial class lstChat : Form
     {
         private TcpClient client;
         private NetworkStream stream;
+        private Thread receiveThread;
+        private object _mainMenu;
 
-        public lstChat()
+        // 1. QUAN TRỌNG: Giữ hàm khởi tạo KHÔNG THAM SỐ để đúng với thiết kế và gọi từ MainClient
+        public lstChat(MainClient mainClient)
         {
-            InitializeComponent();
+            InitializeComponent(); // Nạp đầy đủ các nút bấm, RichTextBox lên giao diện
         }
 
-        // 1. SỬA LẠI HÀM KẾT NỐI (Thêm luồng tự động nhận tin nhắn/file thời gian thực)
-        private void btnConnect_Click(object sender, EventArgs e)
+        // 2. Lấy kết nối từ SocketClient.Instance khi Form bắt đầu tải lên
+        private void lstChat_Load(object sender, EventArgs e)
         {
             try
             {
-                client = new TcpClient();
-                client.Connect(txtIP.Text, int.Parse(txtPort.Text));
+                // Lấy trực tiếp kết nối TCP thực tế từ SocketClient thông qua thuộc tính đã sửa ở Bước 1
+                this.client = SocketClient.Instance.Client;
 
-                stream = client.GetStream();
+                if (this.client != null && this.client.Connected)
+                {
+                    this.stream = this.client.GetStream();
 
-                MessageBox.Show("Kết nối thành công!");
-
-                // Kích hoạt luồng chạy ngầm để liên tục nghe dữ liệu từ Server mà không bị treo giao diện
-                Task.Run(() => ReceiveData());
+                    // Giữ nguyên luồng nhận dữ liệu tự động của bạn
+                    receiveThread = new Thread(ReceiveData);
+                    receiveThread.IsBackground = true;
+                    receiveThread.Start();
+                }
+                else
+                {
+                    rtbMessages.AppendText("⚠️ Lỗi: Chưa kết nối đến Server!" + Environment.NewLine);
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Lỗi kết nối: " + ex.Message);
+                MessageBox.Show("Không thể lấy luồng mạng: " + ex.Message);
             }
         }
+        private void btnBackToMenu_Click(object sender, EventArgs e)
+        {
+            if (_mainMenu != null)
+            {
+                if (_mainMenu is MainClient menu)
+                {
+                    menu.Show();
+                }
+            }
+            this.Close();
+        }
 
-        // 2. GIỮ NGUYÊN HÀM GỬI TIN NHẮN (Bổ sung hiển thị lên ListBox của bạn)
+        // 2. HÀM GỬI TIN NHẮN (Giữ nguyên cấu trúc cờ hiệu 0x01 của bạn)
         private void btnSend_Click(object sender, EventArgs e)
         {
             try
             {
-                string message = txtMessage.Text;
+                if (stream == null) return;
+
+                string message = txtMessage.Text.Trim();
                 if (string.IsNullOrEmpty(message)) return;
 
                 byte[] msgBytes = Encoding.UTF8.GetBytes(message);
                 byte[] lengthBytes = BitConverter.GetBytes(msgBytes.Length);
 
-                stream.WriteByte(0x01); // 0x01: Cờ hiệu nhận diện đây là TIN NHẮN TEXT
+                stream.WriteByte(0x01); // Cờ hiệu nhận diện TIN NHẮN TEXT
                 stream.Write(lengthBytes, 0, 4);
                 stream.Write(msgBytes, 0, msgBytes.Length);
                 stream.Flush();
 
-                // Hiển thị tin nhắn bạn vừa gửi lên listBox1
-                listBox1.Items.Add("Bạn: " + message);
+                rtbMessages.AppendText("Bạn: " + message + Environment.NewLine);
                 txtMessage.Clear();
             }
             catch (Exception ex)
@@ -70,29 +92,25 @@ namespace ClientApp
             }
         }
 
-        // 3. THÊM MỚI HÀM GỬI FILE (Liên kết với nút btnSendFile bạn vừa tạo ở Bước 1)
+        // 3. HÀM GỬI FILE (Giữ nguyên cấu trúc cờ hiệu 0x02 của bạn)
         private void btnSendFile_Click(object sender, EventArgs e)
         {
-            if (client == null || !client.Connected)
+            if (client == null || !client.Connected || stream == null)
             {
                 MessageBox.Show("Vui lòng kết nối đến Server trước!");
                 return;
             }
 
             OpenFileDialog ofd = new OpenFileDialog();
-
-            if (ofd.ShowDialog() != DialogResult.OK)
-                return;
+            if (ofd.ShowDialog() != DialogResult.OK) return;
 
             try
             {
                 string filePath = ofd.FileName;
                 string fileName = Path.GetFileName(filePath);
-
                 byte[] fileData = File.ReadAllBytes(filePath);
 
-                // Header
-                stream.WriteByte(0x02);
+                stream.WriteByte(0x02); // Cờ hiệu nhận diện FILE
 
                 // Độ dài tên file
                 byte[] nameBytes = Encoding.UTF8.GetBytes(fileName);
@@ -107,56 +125,51 @@ namespace ClientApp
 
                 // Nội dung file
                 stream.Write(fileData, 0, fileData.Length);
-
                 stream.Flush();
 
-                listBox1.Items.Add("📤 Đã gửi file: " + fileName);
+                rtbMessages.AppendText("📤 Đã gửi file: " + fileName + Environment.NewLine);
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("Lỗi gửi file: " + ex.Message);
             }
         }
+
+        // Hàm đảm bảo đọc đủ số lượng Byte được yêu cầu trên đường truyền mạng
         private void ReadFull(byte[] buffer, int size)
         {
             int offset = 0;
-
             while (offset < size)
             {
                 int read = stream.Read(buffer, offset, size - offset);
-
                 if (read == 0)
                     throw new Exception("Mất kết nối.");
-
                 offset += read;
             }
         }
 
-
-        // 4. THÊM MỚI LUỒNG NHẬN DỮ LIỆU TỰ ĐỘNG THỜI GIAN THỰC từ Server đổ về
+        // 4. LUỒNG NHẬN DỮ LIỆU TỰ ĐỘNG THỜI GIAN THỰC (Đã sửa lỗi đồng bộ đọc luồng)
         private void ReceiveData()
         {
             try
             {
                 while (client != null && client.Connected)
                 {
-                    // Đọc byte đầu tiên (Header) để phân loại gói tin dữ liệu là gì
                     int header = stream.ReadByte();
-                    if (header == -1) break; // Server ngắt kết nối đột ngột
+                    if (header == -1) break; // Server ngắt kết nối
 
                     if (header == 0x01) // TRƯỜNG HỢP 1: Nhận tin nhắn Text từ Server
                     {
                         byte[] lengthBytes = new byte[4];
-                        stream.Read(lengthBytes, 0, 4);
+                        ReadFull(lengthBytes, 4); // Sửa: Dùng ReadFull thay vì stream.Read để tránh mất gói
                         int msgLen = BitConverter.ToInt32(lengthBytes, 0);
 
                         byte[] msgBytes = new byte[msgLen];
-                        stream.Read(msgBytes, 0, msgLen);
+                        ReadFull(msgBytes, msgLen); // Sửa: Dùng ReadFull đọc trọn vẹn nội dung text
                         string msg = Encoding.UTF8.GetString(msgBytes);
 
-                        // Đồng bộ hiển thị lên ô listBox1 (phải dùng Invoke vì đang ở luồng khác luồng chính)
                         Invoke(new Action(() => {
-                            listBox1.Items.Add("Server: " + msg);
+                            rtbMessages.AppendText("Server: " + msg + Environment.NewLine);
                         }));
                     }
                     else if (header == 0x02) // TRƯỜNG HỢP 2: Nhận File từ Server gửi qua
@@ -164,55 +177,55 @@ namespace ClientApp
                         // Đọc tên File
                         byte[] nameLenBytes = new byte[4];
                         ReadFull(nameLenBytes, 4);
-
                         int nameLen = BitConverter.ToInt32(nameLenBytes, 0);
 
                         byte[] nameBytes = new byte[nameLen];
                         ReadFull(nameBytes, nameLen);
-
                         string fileName = Encoding.UTF8.GetString(nameBytes);
 
-                        // kích thước file
+                        // Đọc kích thước file
                         byte[] sizeBytes = new byte[8];
                         ReadFull(sizeBytes, 8);
-
                         long fileSize = BitConverter.ToInt64(sizeBytes, 0);
 
-                        // dữ liệu file
+                        // Đọc nội dung dữ liệu file
                         byte[] fileBytes = new byte[fileSize];
                         ReadFull(fileBytes, (int)fileSize);
 
+                        // Lưu file vào thư mục thực thi của ứng dụng
                         File.WriteAllBytes(fileName, fileBytes);
 
-                        Invoke(new Action(() =>
-                        {
-                            listBox1.Items.Add("📥 Đã nhận file: " + fileName);
+                        Invoke(new Action(() => {
+                            // Thay vì: rtbMessages.Items.Add("📥 Đã nhận file: " + fileName);
+                            rtbMessages.AppendText("📥 Đã nhận file: " + fileName + Environment.NewLine);
                         }));
                     }
                 }
             }
             catch (Exception)
             {
-                Invoke(new Action(() => {
-                    listBox1.Items.Add("⚠️ Đã mất kết nối tới Server.");
-                }));
+                if (client != null && client.Connected)
+                {
+                    Invoke(new Action(() => {
+                        rtbMessages.AppendText("⚠️ Đã xảy ra lỗi khi nhận dữ liệu." + Environment.NewLine);
+                    }));
+                }
             }
         }
 
-
-        private void label1_Click(object sender, EventArgs e)
+        // Đảm bảo đóng kết nối an toàn khi người dùng tắt Form chat
+        protected override void OnFormClosing(FormClosingEventArgs e)
         {
-            // Để trống hoặc xóa đi nếu bạn đã xóa label cũ
+            // Nếu muốn quay lại menu mà giữ kết nối, chỉ cần không đóng client ở đây.
+            // Ngược lại, nếu muốn ngắt kết nối luôn thì kích hoạt 2 dòng dưới:
+            // stream?.Close();
+            // client?.Close();
+            base.OnFormClosing(e);
         }
 
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtMessage_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+        // Các hàm sự kiện click trống có thể giữ lại để tránh lỗi Designer
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void txtMessage_TextChanged(object sender, EventArgs e) { }
     }
 }
